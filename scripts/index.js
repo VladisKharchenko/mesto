c2cee77100dfdbc94b0dd911e53004831a40c637
Vladislav Kharchenko <har4enko.vladis@yandex.ru>
2023-02-13 00:34:05 +0400

    Проект от 12.02.2023-2

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,37 @@
 const profileEditButton = document.querySelector('.profile__edit-button');
+const popupOpened = document.querySelector('.popup');
 
-profileEditButton.addEventListener('click', function() {
-  const popupOpened = document.querySelector('.popup');
+const popupCloseButton = document.querySelector('.popup__close-button');
+const popupClose = document.querySelector('.popup');
+
+const formElement = document.querySelector('.popup__container');
+const popupInputName = document.querySelector('.popup__input_type_name');
+const popupInputAboutYourself = document.querySelector('.popup__input_type_about-yourself');
+const profileNameElement = document.querySelector('.profile__name');
+const profileAboutYourselfElement = document.querySelector('.profile__about-yourself');
+
+function openPopup () {
   popupOpened.classList.add('popup_opened');
   popupInputName.value = profileNameElement.textContent;
   popupInputAboutYourself.value = profileAboutYourselfElement.textContent;
-});
+}
 
-const popupCloseButton = document.querySelector('.popup__close-button');
+profileEditButton.addEventListener('click', openPopup);
 
-popupCloseButton.addEventListener('click', function() {
-  const popupClose = document.querySelector('.popup');
+function closePopup () {
   popupClose.classList.remove('popup_opened');
-});
+}
 
-const formElement = document.querySelector('.popup__container');
-
-const popupInputName = document.querySelector('.popup__input-name');
-
-const popupInputAboutYourself = document.querySelector('.popup__input-about-yourself');
-
-const profileNameElement = document.querySelector('.profile__name');
-
-const profileAboutYourselfElement = document.querySelector('.profile__about-yourself');
+popupCloseButton.addEventListener('click', closePopup);
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
-
   profileNameElement.textContent = popupInputName.value;
-
   profileAboutYourselfElement.textContent = popupInputAboutYourself.value;
-
+  closePopup();
  }
 
 formElement.addEventListener('submit', handleFormSubmit);
 
-const popupSaveButton = document.querySelector('.popup__save-button');
-
-  popupSaveButton.addEventListener('click', function() {
-    const popupClose = document.querySelector('.popup');
-    popupClose.classList.remove('popup_opened');
-  });
 
 
